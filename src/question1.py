e42ca63b819761d273d561badca8cce475b30da7
Thiago <thiagoneriramos@yahoo.com.br>
2024-04-17 14:31:41 -0300

    Add code to function get_top_N_open_contracts

--- src/question1.py
+++ src/question1.py
@@ -1,5 +1,5 @@
 class Contract:
-    def __init__(self, id, debt):
+    def __init__(self, id: int, debt: float):
         self.id = id
         self.debt = debt
 
@@ -9,4 +9,16 @@ class Contract:
 
 class Contracts:
     def get_top_N_open_contracts(self, open_contracts, renegotiated_contracts, top_n):
-        pass
+        contract_not_reneg = []
+        ids_top_debts = []
+
+        for contract in open_contracts:
+            if contract.id not in renegotiated_contracts:
+                contract_not_reneg.append(contract)
+
+        contract_not_reneg.sort(key=lambda contract: contract.debt, reverse=True)
+
+        for contract in contract_not_reneg[:top_n]:
+            ids_top_debts.append(contract.id)
+
+        return ids_top_debts
