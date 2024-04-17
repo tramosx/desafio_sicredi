2ea925b54ee9d073c21ed54f9dc6b744bf7a0f31
Thiago <thiagoneriramos@yahoo.com.br>
2024-04-17 14:32:13 -0300

    Add function combine_orders

--- src/question2.py
+++ src/question2.py
@@ -1,3 +1,13 @@
 class Orders:
-    def combine_orders(self, requests, n_max):
-        pass
+    def combine_orders(self, requests: list, n_max: int):
+        requests.sort(reverse=True)
+        trips = 0
+
+        start, end = 0, len(requests) - 1
+        while start <= end:
+            if requests[start] + requests[end] <= n_max:
+                end -= 1
+            start += 1
+            trips += 1
+
+        return trips
